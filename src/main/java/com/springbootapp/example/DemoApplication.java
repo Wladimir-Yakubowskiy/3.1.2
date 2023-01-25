2a534a926a2447ebdf93070929be76cc8d4aaa5e
Wladimir-Yakubowskiy <iloveemperor@mail.ru>
2023-01-25 22:53:14 +0300

    импортирована библиотека jakarta.persistence вместо javax.persistence, убраны лишние аннотации

--- src/main/java/com/springbootapp/example/DemoApplication.java
+++ src/main/java/com/springbootapp/example/DemoApplication.java
@@ -2,13 +2,8 @@ package com.springbootapp.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan("com.springbootapp.example.model")
-@EnableJpaRepositories
 public class DemoApplication {
 
 	public static void main(String[] args) {
