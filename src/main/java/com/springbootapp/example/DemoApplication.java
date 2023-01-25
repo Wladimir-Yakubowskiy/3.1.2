aae7daa1841e027f863aee51cdeab4665c06c501
Wladimir-Yakubowskiy <iloveemperor@mail.ru>
2023-01-25 19:54:20 +0300

    second commit

--- src/main/java/com/springbootapp/example/DemoApplication.java
+++ src/main/java/com/springbootapp/example/DemoApplication.java
@@ -2,8 +2,13 @@ package com.springbootapp.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.springbootapp.example.model")
+@EnableJpaRepositories
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/springbootapp/example/repository/UserRepository.java
+++ src/main/java/com/springbootapp/example/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.springbootapp.example.repository;
 
 import com.springbootapp.example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
